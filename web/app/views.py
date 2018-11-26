d03d14c73f846d15d0aaf4e1ea1b4709557ddff0
titxue <xue283866@gmail.com>
2018-11-26 20:43:06 +0800

    添加个首页（未完成）

--- web/app/views.py
+++ web/app/views.py
@@ -20,36 +20,39 @@ key = (
     'b_mid',
     'b_name',
 )
-def get_bilibili():
+def get_bilibili(name,sql,keys):
     try:
-        db = UseSqlite('bilibili.db')
-        results = db.execute('SELECT * FROM bili_video')
+        db = UseSqlite(name)
+        results = db.execute(sql)
         datas = []
         for result in results:
-            data = dict(zip(key,result))
+            data = dict(zip(keys,result))
             datas.append(data)
     except:
-        print("0")
+        pass
     else:
         # data=json.dumps(datas,ensure_ascii=False)
         # print(data[1:len(data)-1])
         return datas
-
-def getpost(code,msg,count,data):
+def get_data(code,msg,count,data):
     json_data = json.dumps({'code':code,'msg':msg,'count':count,'data':data},ensure_ascii=False)
     return json_data
 
 
-posts = get_bilibili()
-data = getpost("0","","100",posts)
+
 @app.route('/')
 @app.route('/index')
 def index():
-
     return render_template('index.html')
 
-@app.route('/api/', methods=['GET'])
+@app.route('/bilibili')
+def bilibili():
+
+    return render_template('bilibili.html')
+
+@app.route('/api/data', methods=['POST'])
 def http_method_example():
-    if request.method == 'GET':
-        return data
+    if request.method == 'POST':
+        api_data = get_bilibili('bilibili.db','SELECT * FROM bili_video',key)
+        return get_data("200","ok",len(api_data),api_data)
 
